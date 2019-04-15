0bf6706599cb0a75ceebcc822fb8d31345fb606e
Chessman97 <47553161+Chessman97@users.noreply.github.com>
2019-04-15 10:50:24 +0700

    Add class Main() and start Parser

---
+++ src/main/java/visitor/Main.java
@@ -0,0 +1,21 @@
+package visitor;
+
+import antlr.CPP14Lexer;
+import antlr.CPP14Parser;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+public class Main {
+    private static String code = "" +
+            "void main(){}";
+
+    public static void main(String[] args) {
+        CPP14Lexer lexer = new CPP14Lexer(CharStreams.fromString(code));
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        CPP14Parser parser = new CPP14Parser(tokens);
+        ParseTree tree = parser.translationunit();
+        new MyVisitor().visit(tree);
+    }
+}
+
---
+++ src/main/java/visitor/MyVisitor.java
@@ -0,0 +1,8 @@
+package visitor;
+
+import org.antlr.v4.runtime.tree.ParseTree;
+
+public class MyVisitor {
+    public void visit(ParseTree tree) {
+    }
+}
