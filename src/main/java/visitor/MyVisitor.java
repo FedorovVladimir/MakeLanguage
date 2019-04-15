15b768ef6cfdd586cbc9da9a89a117e9ca1f6309
Chessman97 <47553161+Chessman97@users.noreply.github.com>
2019-04-15 11:12:42 +0700

    Add class TypeLexeme (for constants) and class Node (for interpret)

--- src/main/java/visitor/MyVisitor.java
+++ src/main/java/visitor/MyVisitor.java
@@ -1,9 +1,14 @@
 package visitor;
 
-import antlr.CPP14BaseListener;
+
 import antlr.CPP14BaseVisitor;
-import org.antlr.v4.runtime.tree.ParseTree;
+import antlr.CPP14Parser;
 
 public class MyVisitor extends CPP14BaseVisitor<Node> {
 
+    @Override
+    public Node visitShiftexpression(CPP14Parser.ShiftexpressionContext ctx) {
+        System.out.println(super.visit(ctx.getChild(2)));
+        return new Node(TypeLexeme.VOID);
+    }
 }
--- src/main/java/visitor/Node.java
+++ src/main/java/visitor/Node.java
@@ -1,4 +1,85 @@
 package visitor;
 
 public class Node {
+
+    private TypeLexeme typeLexeme;
+    private String text;
+
+    public Node(TypeLexeme typeLexeme) {
+        this.typeLexeme = typeLexeme;
+    }
+
+    public Node(TypeLexeme type, String text) {
+        this.typeLexeme = type;
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return "Node " + text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public Node add(Node node) {
+        double a = Double.parseDouble(text);
+        double b = Double.parseDouble(node.getText());
+        return new Node(typeLexeme, String.valueOf(a + b));
+    }
+
+    public Node sub(Node node) {
+        double a = Double.parseDouble(text);
+        double b = Double.parseDouble(node.getText());
+        return new Node(typeLexeme, String.valueOf(a - b));
+    }
+
+    public Node mul(Node node) {
+        double a = Double.parseDouble(text);
+        double b = Double.parseDouble(node.getText());
+        return new Node(typeLexeme, String.valueOf(a * b));
+    }
+
+    public Node div(Node node) {
+        double a = Double.parseDouble(text);
+        double b = Double.parseDouble(node.getText());
+        return new Node(typeLexeme, String.valueOf(a / b));
+    }
+
+    public boolean lessEqual(Node node) {
+        double a = Double.parseDouble(text);
+        double b = Double.parseDouble(node.getText());
+        return a <= b;
+    }
+
+    public boolean largerEqual(Node node) {
+        double a = Double.parseDouble(text);
+        double b = Double.parseDouble(node.getText());
+        return a >= b;
+    }
+
+    public boolean larger(Node node) {
+        double a = Double.parseDouble(text);
+        double b = Double.parseDouble(node.getText());
+        return a > b;
+    }
+
+    public boolean less(Node node) {
+        double a = Double.parseDouble(text);
+        double b = Double.parseDouble(node.getText());
+        return a < b;
+    }
+
+    public boolean equal(Node node) {
+        double a = Double.parseDouble(text);
+        double b = Double.parseDouble(node.getText());
+        return a == b;
+    }
+
+    public boolean notEqual(Node node) {
+        double a = Double.parseDouble(text);
+        double b = Double.parseDouble(node.getText());
+        return a != b;
+    }
 }
