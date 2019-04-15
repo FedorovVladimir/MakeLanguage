0bd9c41d9e2ae2745ed2ac1424c5878f8a505c0d
Chessman97 <47553161+Chessman97@users.noreply.github.com>
2019-04-15 10:56:13 +0700

    Work with Parser Tree

--- src/main/java/visitor/Main.java
+++ src/main/java/visitor/Main.java
@@ -8,7 +8,9 @@ import org.antlr.v4.runtime.tree.ParseTree;
 
 public class Main {
     private static String code = "" +
-            "void main(){}";
+            "void main(){" +
+            "cout << 23;" +
+            "}";
 
     public static void main(String[] args) {
         CPP14Lexer lexer = new CPP14Lexer(CharStreams.fromString(code));
--- src/main/java/visitor/MyVisitor.java
+++ src/main/java/visitor/MyVisitor.java
@@ -1,8 +1,9 @@
 package visitor;
 
+import antlr.CPP14BaseListener;
+import antlr.CPP14BaseVisitor;
 import org.antlr.v4.runtime.tree.ParseTree;
 
-public class MyVisitor {
-    public void visit(ParseTree tree) {
-    }
+public class MyVisitor extends CPP14BaseVisitor<Node> {
+
 }
